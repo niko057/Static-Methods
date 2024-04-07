297be5565137955201caf08aa87f4dc31baaec10
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-07 20:14:33 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Static_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+            arr = arr.Add(1);
+            arr = arr.Add(2);
+            arr = arr.Add(3);
+            arr = arr.Add(4);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine("Cavab:"+sum);
+        }
+       
+    }
+    public static class Extensions
+    {
+        public static int[] Add(this int[] array, int newItem)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = newItem;
+            return array;
+        }
+    }
+
+}
